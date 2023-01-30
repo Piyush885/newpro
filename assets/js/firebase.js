2876a76315bb332e05a85ec62c184e33856e6f8a
unknown <raipiyush@gmail.comgit config --global user.name Piyushgit config --global user.email raipiyush@gmail.com>
2023-01-30 22:31:19 +0530

    first commit

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCGeZ-c-4c-ul86wZJmiuLTwAxNjEGcxGw",
+  authDomain: "fitgo-eab69.firebaseapp.com",
+  projectId: "fitgo-eab69",
+  storageBucket: "fitgo-eab69.appspot.com",
+  messagingSenderId: "155450903686",
+  appId: "1:155450903686:web:f0fa9c8e424373400d9507",
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
